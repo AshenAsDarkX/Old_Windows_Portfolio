ac16d476d8d02badcd28f3c5f16b36ca352f7994
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-24 01:07:46 +0530

    Add popup

--- components/PopupWarning.tsx
+++ components/PopupWarning.tsx
@@ -23,7 +23,7 @@ export default function PopupWarning(props: PopupWarningProps) {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className="" />
-        <Dialog.Content className="fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] border-2 border-b-gray-500 border-l-white border-r-gray-500 border-t-white bg-windows-neutral focus:outline-none">
+        <Dialog.Content className="bg-windows-neutral fixed left-[50%] top-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] border-2 border-b-gray-500 border-l-white border-r-gray-500 border-t-white focus:outline-none">
           <div className="bg-windows-blue flex justify-between p-1 text-white">
             <Dialog.Title className="m-0 text-[17px] font-medium">
               Warning
@@ -31,7 +31,7 @@ export default function PopupWarning(props: PopupWarningProps) {
             <Dialog.Close asChild>
               <button className="" aria-label="Close">
                 <div className="closeButtonHover items-center justify-center border-2 border-solid border-black ">
-                  <div className="border-2 border-b-gray-500 border-l-white border-r-gray-500 border-t-white bg-windows-neutral">
+                  <div className="bg-windows-neutral border-2 border-b-gray-500 border-l-white border-r-gray-500 border-t-white">
                     <p className="border-2 border-dotted border-gray-300 leading-none">
                       <IoCloseSharp />
                     </p>
@@ -49,7 +49,7 @@ export default function PopupWarning(props: PopupWarningProps) {
           <div className="flex justify-end pb-3 pr-3">
             <Dialog.Close asChild>
               <button>
-                <OldButton text="Continue" />
+                <OldButton text="Continue" link={props.link} />
               </button>
             </Dialog.Close>
           </div>
