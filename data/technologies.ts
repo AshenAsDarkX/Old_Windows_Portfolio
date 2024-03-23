3c45c255c2e458aaa48f24288bd2e215aa3a4cbf
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-24 01:00:59 +0530

    Add technologies ts

---
+++ data/technologies.ts
@@ -0,0 +1,74 @@
+type Technologies = {
+  id: number;
+  type: string;
+  name: string;
+  image: string;
+};
+
+const technologies: Technologies[] = [
+  { id: 1, type: "web", name: "React", image: "technologies/react-logo.svg" },
+  { id: 2, type: "web", name: "NextJS", image: "technologies/nextjs-logo.svg" },
+  { id: 3, type: "web", name: "NodeJS", image: "technologies/node-logo.svg" },
+
+  { id: 6, type: "web", name: "MongoDB", image: "technologies/mongo-logo.svg" },
+  {
+    id: 7,
+    type: "web",
+    name: "Tailwind",
+    image: "technologies/tailwind-logo.svg",
+  },
+  { id: 9, type: "web", name: "TypeScript", image: "technologies/ts-logo.svg" },
+  { id: 10, type: "web", name: "PHP", image: "technologies/php-logo.svg" },
+  { id: 12, type: "web", name: "Vanilla", image: "technologies/js-logo.svg" },
+
+  {
+    id: 17,
+    type: "programming",
+    name: "Python",
+    image: "technologies/python-logo.svg",
+  },
+  {
+    id: 18,
+    type: "programming",
+    name: "Java",
+    image: "technologies/java-logo.svg",
+  },
+  {
+    id: 5,
+    type: "mobile",
+    name: "Kotlin",
+    image: "technologies/kotlin-logo.svg",
+  },
+  {
+    id: 13,
+    type: "design",
+    name: "Figma",
+    image: "technologies/figma-logo.svg",
+  },
+  {
+    id: 19,
+    type: "design",
+    name: "Java",
+    image: "technologies/ae-logo.svg",
+  },
+  {
+    id: 14,
+    type: "design",
+    name: "Adobe Il",
+    image: "technologies/ai-logo.svg",
+  },
+  {
+    id: 15,
+    type: "design",
+    name: "Adobe Ps",
+    image: "technologies/ps-logo.svg",
+  },
+  {
+    id: 16,
+    type: "design",
+    name: "Adobe Pr",
+    image: "technologies/pr-logo.svg",
+  },
+];
+
+export default technologies;
