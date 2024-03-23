59b76eaa30d639bc79650751345b9d52bc339f4f
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-24 00:59:07 +0530

    Add projects section

---
+++ app/sections/Projects.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ProjectCard from "@/components/ProjectCard";
+import projects from "@/data/projects";
+
+export default function Projects() {
+  return (
+    <div className="flex flex-col items-center justify-center pt-28">
+      <h2 className="pb-10 text-3xl font-bold lg:text-5xl">Projects</h2>
+      <div className="flex flex-wrap justify-center gap-2">
+        {projects.map((project, key) => {
+          return (
+            <ProjectCard
+              key={key}
+              title={project.title}
+              image={project.image}
+              desc={project.description}
+              text={project.buttonText}
+              link={project.link}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
