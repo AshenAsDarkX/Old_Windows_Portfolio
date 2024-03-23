28c1a4c36e003e143fb4f3389db1d5c7ad4f3f24
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-24 01:09:25 +0530

    Add button component

--- components/OldButton.tsx
+++ components/OldButton.tsx
@@ -6,19 +6,22 @@ type ButtonPropType = {
   text: string;
   className?: string;
   image?: any;
+  selected?:boolean
   link?: string;
 };
 
-export default function OldButton(props: ButtonPropType) {
+export default function OldButton({selected = false,...props}: ButtonPropType) {
   return (
     <a href={props.link} target="_blank">
       <div
         className={clsx(
-          "oldButtonHover cursor-pointer items-center justify-center border-2 border-solid border-black",
+          "oldButtonHover cursor-pointer items-center justify-center border-2 border-solid ",
           props.className,
+          !selected && "border-black",
+          selected && "border-transparent"
         )}
       >
-        <div className="bg-windows-neutral flex items-center border-2 border-b-gray-500 border-l-white border-r-gray-500 border-t-white px-3 py-1">
+        <div className={clsx("bg-windows-neutral flex items-center border-2 px-3 py-1",!selected && "border-b-gray-500 border-l-white border-r-gray-500 border-t-white",selected && "border-t-gray-500 border-r-white border-l-gray-500 border-b-white")}>
           {props.image && (
             <Image width={15} src={props.image} alt={props.text} />
           )}
