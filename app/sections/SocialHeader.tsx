ee5ada224c15c76345b06d5d706f93a7ce08e231
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-23 19:32:38 +0530

    Create social header for desktop

---
+++ app/sections/SocialHeader.tsx
@@ -0,0 +1,31 @@
+import OldButton from "@/components/OldButton";
+import React from "react";
+import linkedInIcon from "@/public/linkedin-icon.svg";
+import GithubIcon from "@/public/github-icon.svg";
+import EmailIcon from "@/public/email-icon.svg";
+
+export default function SocialHeader() {
+  return (
+    <div className="flex w-full top-0 left-0 max-w-7xl pt-5 items-center justify-between">
+        <p className="font-bold text-4xl">himeshana.dev</p>
+        <div className="flex gap-4">
+
+      <OldButton
+        text="LinkedIn"
+        image={linkedInIcon}
+        link="https://www.linkedin.com/in/ashen-himeshana/"
+        />
+      <OldButton
+        text="GitHub"
+        image={GithubIcon}
+        link="https://github.com/AshenAsDarkX"
+        />
+      <OldButton
+        text="Email"
+        image={EmailIcon}
+        link="mailto:ashenhimeshana@gmail.com"
+        />
+        </div>
+    </div>
+  );
+}
