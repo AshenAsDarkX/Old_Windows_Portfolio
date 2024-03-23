e6361a15fb6a423d75f6d5b2683b2da6ea1e4f03
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-24 02:54:12 +0530

    Center footer

--- app/sections/Footer.tsx
+++ app/sections/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mx-auto max-w-6xl px-6 py-4 text-center md:py-4 lg:text-left">
+    <footer className="mx-auto max-w-6xl px-6 py-4 text-center md:py-4">
       © {new Date().getFullYear()} Design & Developed by Ashen
     </footer>
   );
