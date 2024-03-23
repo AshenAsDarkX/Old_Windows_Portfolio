2873f57bf424dbfe27a12d12fa9615975720e421
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-23 19:31:23 +0530

    Add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "windows-blue": "#0302ca",
+        "windows-neutral": "#C0C0C0",
+      },
     },
   },
   plugins: [],
