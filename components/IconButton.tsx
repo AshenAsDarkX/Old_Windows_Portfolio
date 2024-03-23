4370d39dfa5ad23c060ac3e074359f24dd6baac8
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-23 20:16:32 +0530

    Add iconButton

---
+++ components/IconButton.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import Image from "next/image";
+import React from "react";
+
+type IconButtonProps = {
+  isIcon: boolean;
+  iconComponent?: any;
+  className?: string;
+  image?: any;
+  link?: string;
+};
+
+export default function IconButton(props: IconButtonProps) {
+  return (
+    <a href={props.link} target="_blank">
+      <div
+        className={clsx(
+          "oldButtonHover cursor-pointer items-center justify-center border-2 border-solid border-black",
+          props.className,
+        )}
+      >
+        <div className="bg-windows-neutral flex items-center border-2 border-b-gray-500 border-l-white border-r-gray-500 border-t-white p-1">
+          {!props.isIcon && props.image ? (
+            <Image width={15} src={props.image} alt={props.text} />
+          ) : props.isIcon && props.iconComponent ? (
+            <props.iconComponent />
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </a>
+  );
+}
