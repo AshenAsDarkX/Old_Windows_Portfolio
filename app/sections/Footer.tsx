9c681a785f3db464b86928be291863654da00786
Ashen Himeshana <125987832+AshenAsDarkX@users.noreply.github.com>
2024-03-24 00:58:10 +0530

    Add footer sections

---
+++ app/sections/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="p-2">
+      © {new Date().getFullYear()} Design & Developed by Ashen
+    </div>
+  );
+}
